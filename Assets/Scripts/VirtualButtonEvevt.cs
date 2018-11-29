33ffeff25ec8196077f9ccfc592e637931d0d102
hezhangqiang1 <1025440064@qq.com>
2018-11-29 23:53:32 +0800

    VirtualButton

---
+++ Assets/Scripts/VirtualButtonEvevt.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * 实现VirtualButton
+ *
+ *
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonEvevt : MonoBehaviour ,IVirtualButtonEventHandler{
+   
+
+   
+    void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        throw new NotImplementedException();
+    }
+}
