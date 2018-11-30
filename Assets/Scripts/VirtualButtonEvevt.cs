8ab1997461fe1c034df0f56ecf731dda948a5471
hezhangqiang1 <1025440064@qq.com>
2018-11-30 22:23:22 +0800

    VirtualButtonUse

--- Assets/Scripts/VirtualButtonEvevt.cs
+++ Assets/Scripts/VirtualButtonEvevt.cs
@@ -10,26 +10,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class VirtualButtonEvevt : MonoBehaviour ,IVirtualButtonEventHandler{
-   
+public class VirtualButtonEvevt : MonoBehaviour, IVirtualButtonEventHandler {
 
-   
+    VirtualButtonBehaviour [] vbs;
+    public GameObject cube;
+    private bool IsRotion;
     void Start () {
-		
+        vbs = this.GetComponentsInChildren<VirtualButtonBehaviour>();
+        for(int i = 0; i< vbs.Length; i++)
+        {
+            vbs[i].RegisterEventHandler(this);
+        }
 	}
 	
 	
 	void Update () {
-		
+        if (IsRotion)
+        {
+            cube.transform.Rotate(Vector3.up, 60f, Space.World);
+        }
 	}
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
-        throw new NotImplementedException();
+        switch (vb.VirtualButtonName)
+        {
+            case "Rotate":
+                IsRotion = true;
+                break;
+            case "Large":
+                cube.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+                break;
+               
+        }
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
-        throw new NotImplementedException();
+        switch (vb.VirtualButtonName)
+        {
+            case "Rotion":
+                IsRotion = false ;
+                break;
+            case "Large":
+                break;
+
+        }
+
     }
 }
